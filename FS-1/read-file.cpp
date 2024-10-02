ccce39eb522b13c345c39018a480880c44e507a0
sona-avagyan <sona.avagyan@student.rau.am>
2024-10-02 21:11:47 +0400

    read-file.cpp

--- FS-1/read-file.cpp
+++ FS-1/read-file.cpp
@@ -6,7 +6,7 @@
 
 int main(int argc, char* argv[]) {
     if (argc != 2) {
-        std::cerr << "Usage: " << argv[0] << " <file_path>" << std::endl;
+        std::cerr << "Usage: " << argv[0];
         return 1;
     }
 
