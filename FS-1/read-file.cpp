5b7269a2e165d4aca3b778d054c7676f72722161
sona-avagyan <sona.avagyan@student.rau.am>
2024-10-02 14:38:59 +0400

    Update read-file.cpp

--- FS-1/read-file.cpp
+++ FS-1/read-file.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <fcntl.h>  
 #include <unistd.h>
-#include <cstring> 
+#include <string> 
 #include <errno.h>
 
 int main(int argc, char* argv[]) {
@@ -12,7 +12,7 @@ int main(int argc, char* argv[]) {
 
     int fd = open(argv[1], O_RDONLY);
     if (fd == -1) {
-        std::cerr << "Error: Could not open file " << argv[1] << ": " << strerror(errno) << std::endl;
+        std::cerr << "Error" << argv[1] << ": " << strerror(errno) << std::endl;
         return 1;
     }
 
@@ -22,20 +22,20 @@ int main(int argc, char* argv[]) {
     ssize_t bytes_read;
     while ((bytes_read = read(fd, buffer, buffer_size)) > 0) {
         if (write(STDOUT_FILENO, buffer, bytes_read) == -1) {
-            std::cerr << "Error: Failed to write to stdout: " << strerror(errno) << std::endl;
+            std::cerr << "Error" << strerror(errno) << std::endl;
             close(fd);
             return 1;
         }
     }
 
     if (bytes_read == -1) {
-        std::cerr << "Error: Failed to read from file " << argv[1] << ": " << strerror(errno) << std::endl;
+        std::cerr << "Error" << argv[1] << ": " << strerror(errno) << std::endl;
         close(fd);
         return 1;
     }
 
     if (close(fd) == -1) {
-        std::cerr << "Error: Failed to close file " << argv[1] << ": " << strerror(errno) << std::endl;
+        std::cerr << "Error" << argv[1] << ": " << strerror(errno) << std::endl;
         return 1;
     }
 
